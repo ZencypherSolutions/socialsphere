23bd3697771b2076aadd30d4235fff85df14c3db
Sarthak Sharma <sarthaksharms9@gmail.com>
2025-02-20 17:44:54 +0530

    adjusted padding

--- src/app/pages/LoginPage/LoginPage.tsx
+++ src/app/pages/LoginPage/LoginPage.tsx
@@ -4,19 +4,19 @@ const LoginPage = () => {
     return (
         
         <div className="flex items-center justify-center w-full h-[100vh] bg-[#EEEEEE] font-inter">
-            <div className="flex flex-col gap-8 w-[350px] md:w-[480px] bg-[#FFFFFF] py-10 px-14 rounded-[40px] shadow-xl text-center">
-                <div className="flex flex-col gap-2">
+            <div className="flex flex-col gap-8 w-[350px] md:w-[480px] bg-[#FFFFFF] p-10 rounded-[40px] shadow-xl text-center">
+                <div className="flex flex-col items-center justify-center gap-2">
                     <div className="text-[40px] font-semibold text-[#323643] [text-shadow:2px_2px_4px_rgba(0,0,0,0.2)]">Sign In</div>
-                    <div className="text-[#323643] font-medium text-sm">
+                    <div className="w-[90%] font-medium text-sm text-[#323643]">
                         Please enter a username for your account and connect your wallet
                     </div>
                 </div>
 
-                <div className="flex flex-col gap-4">
+                <div className="flex flex-col items-center justify-center gap-4">
                     <input type="text" placeholder="Enter new username"
-                        className="w-full px-4 py-2 border-[1px] border-[#7D7D7D] rounded-[20px] text-[#7D7D7D] text-center focus:outline-none"/>
+                        className="w-[90%] px-4 py-2 border-[1px] border-[#7D7D7D] rounded-[20px] text-[#7D7D7D] text-center focus:outline-none"/>
 
-                    <button className="w-full flex items-center justify-center gap-2 px-4 py-2 bg-[#4151FF] text-white font-semibold rounded-[20px]">
+                    <button className="w-[90%] flex items-center justify-center gap-2 px-4 py-2 bg-[#4151FF] text-white font-semibold rounded-[20px]">
                         <img src="/connect-wallet.png" className="w-5 h-5"/>
                         Connect Wallet
                     </button>
