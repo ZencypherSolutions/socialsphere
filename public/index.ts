ca4ae9567c581a38e8452721f68088cbf8250568
Ezeh20 <ranter1570@gmail.com>
2025-02-25 23:44:55 +0000

    feat: added and staged the introduction modal

---
+++ public/index.ts
@@ -0,0 +1,5 @@
+import intro1 from './intro1.png'
+import intro2 from './intro2.png'
+import intro3 from './intro3.png'
+
+export { intro1, intro2, intro3}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,15 @@ import Header from "@/component/Header";
 import Head from "next/head";
 import LandingPage from "./pages/LandingPage/LandingPage";
 import LogInPage from "./pages/Log-InPage/LogInPage";
+import IntroductionModal from "@/components/introduction-modal";
 
 export default function Home() {
   return (
     <>
       <div>
-        <LogInPage/>
+ 
+        <LandingPage />
+       <LogInPage/>
      </div>
     </>
   );
