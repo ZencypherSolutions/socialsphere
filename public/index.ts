deef1a2bd09b59da3aa11d44e8dac7ade0309dd6
Ezeh20 <ranter1570@gmail.com>
2025-03-03 15:26:11 +0000

    chore: renamed the modal assets

--- public/index.ts
+++ public/index.ts
@@ -1,5 +1,5 @@
-import intro1 from './intro1.png'
-import intro2 from './intro2.png'
-import intro3 from './intro3.png'
+import intro1 from './intro-modal-1.png'
+import intro2 from './intro-modal-2.png'
+import intro3 from './intro-modal-3.png'
 
 export { intro1, intro2, intro3}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,19 @@
 // pages/index.js
+"use client"
 import Header from "@/component/Header";
 import Head from "next/head";
 import LandingPage from "./pages/LandingPage/LandingPage";
 import LogInPage from "./pages/Log-InPage/LogInPage";
 import IntroductionModal from "@/components/introduction-modal";
+import { useState } from "react";
 
 export default function Home() {
-  return (
-    <>
-      <div>
- 
-     <LogInPage/>
-     </div>
-    </>
-  );
+  const [isOpen, setIsOpen] = useState(true)
+	return (
+		<>
+			<div>
+				<IntroductionModal onOpen={isOpen} />
+			</div>
+		</>
+	);
 }
